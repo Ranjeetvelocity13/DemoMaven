f37ede400889ea62e3a4f8cd738cd0f5fd1bd5c3
Ranjeet <kendreranjet07@gmail.com>
2022-07-10 23:17:36 +0530

    First commit

---
+++ src/test/java/Demo/TestDemo.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/Demo/Testdemo1.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Testdemo1 {
+
+	
+	@Test
+	public void m1() {
+		System.out.println("Hello Good Morning");
+	}
+}
